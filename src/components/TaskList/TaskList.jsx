ce3b346eec64fe840e130b79f80a8c54c72020a6
bhagatankit05 <bhagatankit82@gmail.com>
2025-06-19 19:35:31 +0530

    update tasklist

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -9,19 +9,23 @@ const TaskList = ({data}) => {
   return (
     <div
       id="tasklist"
-      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full py-5 mt-10 px-4"
-    >
-      {/* Task 1 - High Priority */}
-      <AcceptTask/>
-
-      {/* Task 2 - Medium Priority */}
-      <NewTask/>
-
-      {/* Task 3 - Urgent */}
-       <CompleteTask/>     
-
-      {/* Task 4 - Low Priority */}
-      <FailedTask/>
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full py-5 mt-10 px-4">
+        {
+          data.tasks.map((elem)=>{
+              if(elem.active){
+                return </AcceptTask>
+             }
+             if(elem.newTask){
+              return <NewTask/>
+             }
+             if(elem.completed){
+              return <CompleteTask/>
+             }
+             if(elem.failed){
+              return <FailedTask/>
+             }
+          })
+        }
     </div>
   );
 };
